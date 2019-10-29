a9b7fd3ec3a80d6a4a14ff191ff2ed5ba42ddf93
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-29 17:07:33 +0000

    resizes the flag

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -6,9 +6,8 @@ class CountryDetail extends Component {
     if (!this.props.country) return null;
     return (
       <React.Fragment>
-      <p>{this.props.country.name}</p>
-      <img src={this.props.country.flag} alt="flag"/>
-
+      <p>Country name: {this.props.country.name}</p>
+      <img src={this.props.country.flag} alt="flag" height="400" width="500"/>
       </React.Fragment>
     )
   }
